03194dcde6aa17e160bb26edbb752d434ded3c15
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2009-11-21 03:46:38 +0000

    MagicPwd V1.0.0.0 项目初始化。

---
+++ trunk/MagicPwd/src/com/magicpwd/MagicPwd.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.magicpwd;
+
+/**
+ *
+ * @author Amon
+ */
+public class MagicPwd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
