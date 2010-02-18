7732a85bf61d05320a8ab263fe6fa77905be8973
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-02-18 18:04:36 +0000

    安装功能实现。

--- MagicPwd/setup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@set "cp=%cd%\magicpwd.jar"
-@set "dp=%homedrive%%homepath%\桌面\魔方密码0.lnk"
-@set "ip=%cd%\logo\logo.ico"
-@set "dt=魔方密码，跨平台密码管理软件！"
-@os\win\ShortCut.exe /F:"%dp%" /A:C /T:"%cp%" /R:1 /I:"%ip%",0 /D:"%dt%"
-@os\win\MessageBox.exe	/C:安装成功 /M:魔方密码安装成功！ /W:10 /T:64
