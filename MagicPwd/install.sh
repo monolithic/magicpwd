ff2063677347fe86e57043cace36c02700e2bb0e
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-02-28 04:23:22 +0000

    Linux安装脚本。

--- MagicPwd/install.sh
+++ MagicPwd/install.sh
@@ -1,4 +1,33 @@
-if [ -e "${filename}" ] then
-echo "" > a.desktop
+if [ -e "magicpwd.desktop" ]
+then
+echo "#!/usr/bin/env xdg-open" > magicpwd.desktop
+else
+echo "#!/usr/bin/env xdg-open" >> magicpwd.desktop
 fi
-echo "" >> a.desktop
+
+echo "" >> magicpwd.desktop
+echo "[Desktop Entry]" >> magicpwd.desktop
+echo "Encoding=UTF-8" >> magicpwd.desktop
+echo "Version=1.0" >> magicpwd.desktop
+echo "Type=Application" >> magicpwd.desktop
+echo "Terminal=false" >> magicpwd.desktop
+echo "Icon[zh_CN]=evince" >> magicpwd.desktop
+echo "Exec=$(pwd)/magicpwd.jar" >> magicpwd.desktop
+echo "Name[zh_CN]=魔方密码" >> magicpwd.desktop
+echo "Comment[zh_CN]=魔方密码" >> magicpwd.desktop
+echo "Name=魔方密码" >> magicpwd.desktop
+echo "Comment=魔方密码" >> magicpwd.desktop
+echo "Icon=evince" >> magicpwd.desktop
+echo "" >> magicpwd.desktop
+
+if [ -d ~/desktop/ ]
+then
+mv magicpwd.desktop ~/desktop/magicpwd.desktop
+chmod 711 ~/desktop/magicpwd.desktop
+fi
+
+if [ -d ~/桌面/ ]
+then
+mv magicpwd.desktop ~/桌面/magicpwd.desktop
+chmod 711 ~/桌面/magicpwd.desktop
+fi
