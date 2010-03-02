0eaa45461dc26cfaceba89545bfbff13bb1fe55a
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-03-02 14:20:58 +0000

    徽标功能完善。

--- trunk/MagicPwd/src/com/magicpwd/x/IcoDialog.java
+++ trunk/MagicPwd/src/com/magicpwd/x/IcoDialog.java
@@ -4,10 +4,12 @@
  */
 package com.magicpwd.x;
 
+import com.magicpwd.MagicPwd;
 import com.magicpwd._cons.ConsEnv;
 import com.magicpwd._face.IBackCall;
 import com.magicpwd._util.Util;
 import com.magicpwd.r.AmonFF;
+import java.util.ArrayList;
 
 /**
  *
@@ -17,53 +19,137 @@ public class IcoDialog extends javax.swing.JDialog
 {
 
     private java.io.File icoPath;
-    private java.util.Map<String, javax.swing.ImageIcon> iconList;
+    private java.util.List<javax.swing.JLabel> iconList;
     private IBackCall backCall;
 
     public IcoDialog(IBackCall backCall)
     {
+        super(MagicPwd.getCurrForm(), true);
         this.backCall = backCall;
     }
 
     public void initView()
     {
+
+        javax.swing.JScrollPane sp_IconList = new javax.swing.JScrollPane();
+        pl_IconList = new javax.swing.JPanel();
+        bt_Append = new javax.swing.JButton();
+        bt_Select = new javax.swing.JButton();
+
+        pl_IconList.setLayout(new java.awt.GridLayout(0, 10));
+        pl_IconList.setBackground(java.awt.Color.WHITE);
+        sp_IconList.setViewportView(pl_IconList);
+
+        bt_Append.addActionListener(new java.awt.event.ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                bt_AppendActionPerformed(evt);
+            }
+        });
+
+        bt_Select.addActionListener(new java.awt.event.ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                bt_SelectActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this.getContentPane());
+        this.getContentPane().setLayout(layout);
+        javax.swing.GroupLayout.SequentialGroup hsg1 = layout.createSequentialGroup();
+        hsg1.addComponent(bt_Select);
+        hsg1.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+        hsg1.addComponent(bt_Append);
+        javax.swing.GroupLayout.ParallelGroup hpg = layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING);
+        hpg.addComponent(sp_IconList, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE);
+        hpg.addGroup(hsg1);
+        javax.swing.GroupLayout.SequentialGroup hsg = layout.createSequentialGroup();
+        hsg.addContainerGap();
+        hsg.addGroup(hpg);
+        hsg.addContainerGap();
+        layout.setHorizontalGroup(hsg);
+
+        javax.swing.GroupLayout.ParallelGroup vpg = layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE);
+        vpg.addComponent(bt_Append);
+        vpg.addComponent(bt_Select);
+        javax.swing.GroupLayout.SequentialGroup vsg = layout.createSequentialGroup();
+        vsg.addContainerGap();
+        vsg.addComponent(sp_IconList, javax.swing.GroupLayout.DEFAULT_SIZE, 241, Short.MAX_VALUE);
+        vsg.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+        vsg.addGroup(vpg);
+        vsg.addContainerGap();
+        layout.setVerticalGroup(vsg);
+
+        this.pack();
+        this.setResizable(false);
+        Util.centerForm(this, MagicPwd.getCurrForm());
     }
 
     public void initLang()
     {
+        bt_Append.setText("jButton1");
+        bt_Select.setText("jButton2");
     }
 
     public void initData()
     {
+        if (iconList == null)
+        {
+            if (icoPath == null)
+            {
+                icoPath = new java.io.File(ConsEnv.DIR_DAT, ConsEnv.DIR_ICO);
+                if (!icoPath.exists())
+                {
+                    icoPath.mkdirs();
+                }
+            }
+
+            java.io.File[] fileList = icoPath.listFiles(new AmonFF("[0-9a-z]{16}\\.png", false));
+            if (fileList == null)
+            {
+                return;
+            }
+
+            javax.swing.JLabel label;
+            iconList = new java.util.ArrayList<javax.swing.JLabel>(fileList.length);
+            for (java.io.File file : fileList)
+            {
+                if (!file.isFile())
+                {
+                    continue;
+                }
+                label = new javax.swing.JLabel(new javax.swing.ImageIcon(Util.getImage(file.getAbsolutePath())));
+                iconList.add(label);
+                pl_IconList.add(label);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < iconList.size(); i += 1)
+            {
+                pl_IconList.add(iconList.get(i));
+            }
+        }
     }
 
     private synchronized void getIcon()
     {
-        icoPath = new java.io.File(ConsEnv.DIR_DAT, ConsEnv.DIR_ICO);
-        if (!icoPath.exists())
-        {
-            icoPath.mkdirs();
-        }
-
-        java.io.File[] fileList = icoPath.listFiles(new AmonFF("[0-9a-z]{16}\\.png", false));
-        if (fileList == null)
-        {
-            return;
-        }
-
-        for (java.io.File file : fileList)
-        {
-            iconList.put(file.getName().split("\\.")[0], new javax.swing.ImageIcon(Util.getImage(file.getAbsolutePath())));
-        }
     }
 
-    public javax.swing.ImageIcon getLogo(String name)
+    private void bt_SelectActionPerformed(java.awt.event.ActionEvent evt)
     {
-        if (iconList == null)
-        {
-            iconList = new java.util.HashMap<String, javax.swing.ImageIcon>();
-            getIcon();
-        }
-        return iconList.get(name);
     }
+
+    private void bt_AppendActionPerformed(java.awt.event.ActionEvent evt)
+    {
+    }
+    private javax.swing.JButton bt_Append;
+    private javax.swing.JButton bt_Select;
+    private javax.swing.JPanel pl_IconList;
 }
