c1f5f4cd61fa488fa65939c5ee1d741fb7f3e6ef
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-03-07 04:36:55 +0000

    用户配置功能完善。

--- MagicPwd/src/com/magicpwd/x/IcoDialog.java
+++ MagicPwd/src/com/magicpwd/x/IcoDialog.java
@@ -18,6 +18,10 @@ import com.magicpwd.r.AmonFF;
 public class IcoDialog extends javax.swing.JDialog
 {
 
+    private java.awt.Color bdColor;
+    private java.awt.Color bsColor;
+    private java.awt.Color fdColor;
+    private java.awt.Color fsColor;
     private java.io.File icoPath;
     private IBackCall backCall;
     private javax.swing.JLabel lb_LastIcon;
@@ -26,6 +30,11 @@ public class IcoDialog extends javax.swing.JDialog
     {
         super(MagicPwd.getCurrForm(), true);
         this.backCall = backCall;
+        javax.swing.UIDefaults uiDef = javax.swing.UIManager.getDefaults();
+        bdColor = uiDef.getColor("Table.background");
+        bsColor = uiDef.getColor("Table.selectionBackground");
+        fdColor = uiDef.getColor("Table.foreground");
+        fsColor = uiDef.getColor("Table.selectionForeground");
     }
 
     public void initView()
@@ -36,24 +45,18 @@ public class IcoDialog extends javax.swing.JDialog
         bt_Append = new javax.swing.JButton();
         bt_Select = new javax.swing.JButton();
 
-        pl_IconList.setBackground(javax.swing.UIManager.getDefaults().getColor("Table.background"));
+        pl_IconList.setBackground(bdColor);
         pl_IconList.setLayout(new javax.swing.BoxLayout(pl_IconList, javax.swing.BoxLayout.Y_AXIS));
         sp_IconList.setViewportView(pl_IconList);
 
-        pl_IconGrid.setLayout(new java.awt.GridLayout(0, 10));
+        pl_IconGrid.setLayout(new java.awt.GridLayout(0, 10, 4, 4));
         pl_IconGrid.setOpaque(false);
         javax.swing.GroupLayout pl_IconListLayout = new javax.swing.GroupLayout(pl_IconList);
         pl_IconList.setLayout(pl_IconListLayout);
         pl_IconListLayout.setHorizontalGroup(
-            pl_IconListLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pl_IconGrid, javax.swing.GroupLayout.DEFAULT_SIZE, 260, Short.MAX_VALUE)
-        );
+                pl_IconListLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(pl_IconGrid, javax.swing.GroupLayout.DEFAULT_SIZE, 260, Short.MAX_VALUE));
         pl_IconListLayout.setVerticalGroup(
-            pl_IconListLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pl_IconListLayout.createSequentialGroup()
-                .addComponent(pl_IconGrid, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(251, Short.MAX_VALUE))
-        );
+                pl_IconListLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(pl_IconListLayout.createSequentialGroup().addComponent(pl_IconGrid, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE).addContainerGap(251, Short.MAX_VALUE)));
 
         bt_Append.addActionListener(new java.awt.event.ActionListener()
         {
@@ -100,7 +103,7 @@ public class IcoDialog extends javax.swing.JDialog
         vsg.addGroup(vpg);
         vsg.addContainerGap();
         layout.setVerticalGroup(vsg);
-        
+
         this.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         this.setIconImage(Util.getLogo());
         this.setResizable(false);
@@ -140,13 +143,35 @@ public class IcoDialog extends javax.swing.JDialog
             }
         }
 
+        java.awt.event.MouseAdapter listener = new java.awt.event.MouseAdapter()
+        {
+
+            @Override
+            public void mouseReleased(java.awt.event.MouseEvent evt)
+            {
+                lb_LastIcon.setBackground(bdColor);
+                lb_LastIcon.setForeground(fdColor);
+                lb_LastIcon = (javax.swing.JLabel) evt.getSource();
+                lb_LastIcon.setBackground(bsColor);
+                lb_LastIcon.setForeground(fsColor);
+            }
+        };
+
         boolean checked = !Util.isValidate(lastIcon) || "0".equals(lastIcon);
         javax.swing.JLabel label = new javax.swing.JLabel(Util.getNone());
+        label.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        label.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        label.setBackground(bdColor);
+        label.setOpaque(true);
+        label.setText("0");
+        label.putClientProperty("key", "0");
+        label.addMouseListener(listener);
         pl_IconGrid.add(label);
         if (checked)
         {
             lb_LastIcon = label;
-            lb_LastIcon.setBackground(java.awt.Color.BLUE);
+            lb_LastIcon.setBackground(bsColor);
+            lb_LastIcon.setForeground(fsColor);
         }
 
         java.io.File[] fileList = icoPath.listFiles(new AmonFF("[0-9a-z]{16}\\.png", false));
@@ -155,19 +180,8 @@ public class IcoDialog extends javax.swing.JDialog
             return;
         }
 
-        java.awt.event.MouseAdapter listener = new java.awt.event.MouseAdapter()
-        {
-
-            @Override
-            public void mouseReleased(java.awt.event.MouseEvent evt)
-            {
-                lb_LastIcon.setBackground(pl_IconList.getBackground());
-                lb_LastIcon = (javax.swing.JLabel) evt.getSource();
-                lb_LastIcon.setBackground(java.awt.Color.BLUE);
-            }
-        };
-
         java.util.regex.Pattern pattern = java.util.regex.Pattern.compile("[0-9A-Za-z]{16}");
+        int i = 1;
         for (java.io.File file : fileList)
         {
             if (!file.isFile())
@@ -182,6 +196,11 @@ public class IcoDialog extends javax.swing.JDialog
 
             String key = matcher.group();
             label = new javax.swing.JLabel(Util.getIcon(file));
+            label.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+            label.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+            label.setText(Integer.toString(i++));
+            label.setBackground(bdColor);
+            label.setForeground(fdColor);
             label.setOpaque(true);
             label.putClientProperty("key", key);
             label.addMouseListener(listener);
@@ -192,7 +211,8 @@ public class IcoDialog extends javax.swing.JDialog
                 if (checked)
                 {
                     lb_LastIcon = label;
-                    lb_LastIcon.setBackground(java.awt.Color.BLUE);
+                    label.setBackground(bsColor);
+                    label.setForeground(fsColor);
                 }
             }
         }
