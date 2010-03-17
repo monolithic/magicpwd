35eaf42220ceddeaff438a39011ac1c480c76fbb
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-03-17 12:42:04 +0000

    用户配置功能完善。

--- MagicPwd/src/com/magicpwd/u/DBU3000.java
+++ MagicPwd/src/com/magicpwd/u/DBU3000.java
@@ -4,12 +4,19 @@
  */
 package com.magicpwd.u;
 
+import com.magicpwd._cons.ConsEnv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
  * @author Amon
  */
 public class DBU3000
 {
+
     public void initView()
     {
     }
@@ -19,6 +26,33 @@ public class DBU3000
     }
 
     public void initData()
+    {
+        try
+        {
+            Class.forName("org.hsqldb.jdbcDriver");
+
+            Connection connSrc1 = DriverManager.getConnection("jdbc:hsqldb:file:" + ConsEnv.DIR_DAT + "/amon");
+            Statement statSrc1 = connSrc1.createStatement();
+            Statement statSrc2 = connSrc1.createStatement();
+
+            Connection connDst = DriverManager.getConnection("jdbc:hsqldb:file:" + ConsEnv.DIR_DAT + "/amon");
+            Statement statDst = connDst.createStatement();
+        }
+        catch (Exception exp)
+        {
+            exp.printStackTrace();
+        }
+    }
+
+    private void copyPwds(Statement statSrc1, Statement statSrc2, Statement statDst) throws Exception
+    {
+        ResultSet result = statSrc1.executeQuery("");
+        while (result.next())
+        {
+        }
+    }
+
+    public static void main(String[] args)
     {
     }
 }
