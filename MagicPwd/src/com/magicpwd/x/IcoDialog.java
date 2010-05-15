06ac0b7588ad956f65b0827ff64d7783509a5438
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-05-15 04:02:56 +0000

    多语言支持。

--- MagicPwd/src/com/magicpwd/x/IcoDialog.java
+++ MagicPwd/src/com/magicpwd/x/IcoDialog.java
@@ -22,41 +22,21 @@ import com.magicpwd.r.AmonFF;
 public class IcoDialog extends javax.swing.JDialog
 {
 
-    private java.awt.Color bdColor;
-    private java.awt.Color bsColor;
-    private java.awt.Color fdColor;
-    private java.awt.Color fsColor;
+    private IcoModel icoModel;
     private java.io.File filePath;
-    private java.io.File icoPath;
+    private java.io.File iconPath;
     private IBackCall backCall;
     private IcoLabel lb_LastIcon;
-    private java.awt.event.ActionListener listener;
 
     public IcoDialog(IBackCall backCall)
     {
         super(MagicPwd.getCurrForm(), true);
         this.backCall = backCall;
-        javax.swing.UIDefaults uiDef = javax.swing.UIManager.getDefaults();
-        bdColor = uiDef.getColor("Table.background");
-        bsColor = uiDef.getColor("Table.selectionBackground");
-        fdColor = uiDef.getColor("Table.foreground");
-        fsColor = uiDef.getColor("Table.selectionForeground");
     }
 
     public void initView()
     {
         bt_Append = new javax.swing.JButton();
-        bt_Select = new javax.swing.JButton();
-        javax.swing.JScrollPane sp_IconGrid = new javax.swing.JScrollPane();
-        tb_IconGrid = new javax.swing.JTable();
-
-        tb_IconGrid.setModel(null);
-        tb_IconGrid.getTableHeader().setVisible(false);
-        tb_IconGrid.setCellSelectionEnabled(true);
-        tb_IconGrid.setShowHorizontalLines(false);
-        tb_IconGrid.setShowVerticalLines(false);
-        sp_IconGrid.setViewportView(tb_IconGrid);
-
         bt_Append.addActionListener(new java.awt.event.ActionListener()
         {
 
@@ -67,6 +47,7 @@ public class IcoDialog extends javax.swing.JDialog
             }
         });
 
+        bt_Select = new javax.swing.JButton();
         bt_Select.addActionListener(new java.awt.event.ActionListener()
         {
 
@@ -77,31 +58,40 @@ public class IcoDialog extends javax.swing.JDialog
             }
         });
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(sp_IconGrid, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 280, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(bt_Select)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(bt_Append)))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(sp_IconGrid, javax.swing.GroupLayout.DEFAULT_SIZE, 251, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(bt_Append)
-                    .addComponent(bt_Select))
-                .addContainerGap())
-        );
+        javax.swing.JScrollPane sp_IconGrid = new javax.swing.JScrollPane();
+        tb_IconGrid = new javax.swing.JTable();
+        tb_IconGrid.getTableHeader().setVisible(false);
+        tb_IconGrid.setCellSelectionEnabled(true);
+        tb_IconGrid.setShowHorizontalLines(false);
+        tb_IconGrid.setShowVerticalLines(false);
+        sp_IconGrid.setViewportView(tb_IconGrid);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this.getContentPane());
+        this.getContentPane().setLayout(layout);
+
+        javax.swing.GroupLayout.SequentialGroup hsg1 = layout.createSequentialGroup();
+        hsg1.addComponent(bt_Select);
+        hsg1.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+        hsg1.addComponent(bt_Append);
+        javax.swing.GroupLayout.ParallelGroup hpg1 = layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING);
+        hpg1.addComponent(sp_IconGrid, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 280, Short.MAX_VALUE);
+        hpg1.addGroup(hsg1);
+        javax.swing.GroupLayout.SequentialGroup hsg2 = layout.createSequentialGroup();
+        hsg2.addContainerGap();
+        hsg2.addGroup(hpg1);
+        hsg2.addContainerGap();
+        layout.setHorizontalGroup(hsg2);
+
+        javax.swing.GroupLayout.ParallelGroup vpg1 = layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE);
+        vpg1.addComponent(bt_Append);
+        vpg1.addComponent(bt_Select);
+        javax.swing.GroupLayout.SequentialGroup vsg1 = layout.createSequentialGroup();
+        vsg1.addContainerGap();
+        vsg1.addComponent(sp_IconGrid, javax.swing.GroupLayout.DEFAULT_SIZE, 251, Short.MAX_VALUE);
+        vsg1.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+        vsg1.addGroup(vpg1);
+        vsg1.addContainerGap();
+        layout.setVerticalGroup(vsg1);
 
         this.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         this.setIconImage(Util.getLogo());
@@ -119,86 +109,22 @@ public class IcoDialog extends javax.swing.JDialog
 
     public void initData(final String lastIcon)
     {
+        if (icoModel == null)
+        {
+            icoModel = new IcoModel();
+            tb_IconGrid.setModel(icoModel);
+        }
         javax.swing.SwingUtilities.invokeLater(new Runnable()
         {
 
             @Override
             public void run()
             {
-                tb_IconGrid.removeAll();
-                initIcon(lastIcon);
+                icoModel.initIcon(iconPath, lastIcon);
+                tb_IconGrid.setRowSelectionInterval(0, 0);
+                tb_IconGrid.setColumnSelectionInterval(0, 0);
             }
         });
-    }
-
-    private synchronized void initIcon(String lastIcon)
-    {
-        if (icoPath == null)
-        {
-            icoPath = Util.icoPath;
-            if (!icoPath.exists())
-            {
-                icoPath.mkdirs();
-            }
-        }
-
-        if (listener == null)
-        {
-            listener = new java.awt.event.ActionListener()
-            {
-
-                @Override
-                public void actionPerformed(java.awt.event.ActionEvent evt)
-                {
-                    selectIcoLabel((IcoLabel) evt.getSource());
-                }
-            };
-        }
-
-        boolean checked = !Util.isValidate(lastIcon) || "0".equals(lastIcon);
-        IcoLabel label = createIcoLabel("0", "0", Util.getNone(), listener);
-        tb_IconGrid.add(label);
-        if (checked)
-        {
-            lb_LastIcon = label;
-            lb_LastIcon.setBackground(bsColor);
-            lb_LastIcon.setForeground(fsColor);
-        }
-
-        java.io.File[] fileList = icoPath.listFiles(new AmonFF("[0-9a-z]{16}\\.png", false));
-        if (fileList == null)
-        {
-            return;
-        }
-
-        java.util.regex.Pattern pattern = java.util.regex.Pattern.compile("[0-9A-Za-z]{16}");
-        int i = 1;
-        for (java.io.File file : fileList)
-        {
-            if (!file.isFile())
-            {
-                continue;
-            }
-            java.util.regex.Matcher matcher = pattern.matcher(file.getName());
-            if (!matcher.find())
-            {
-                continue;
-            }
-
-            String key = matcher.group();
-            label = createIcoLabel(key, Integer.toString(i++), Util.getIcon(key), listener);
-            tb_IconGrid.add(label);
-            if (!checked)
-            {
-                checked = key.equalsIgnoreCase(lastIcon);
-                if (checked)
-                {
-                    lb_LastIcon = label;
-                    label.setBackground(bsColor);
-                    label.setForeground(fsColor);
-                }
-            }
-        }
     }
 
     private void bt_SelectActionPerformed(java.awt.event.ActionEvent evt)
@@ -243,39 +169,10 @@ public class IcoDialog extends javax.swing.JDialog
             Lang.showMesg(this, LangRes.P30F7A05, "无法读取您选择的文件，请确认您是否有足够的权限！");
             return;
         }
+
         try
         {
-            java.io.FileInputStream fis = new java.io.FileInputStream(filePath);
-            java.awt.image.BufferedImage img = javax.imageio.ImageIO.read(fis);
-            fis.close();
-
-            final int S = 16;
-            int w = img.getWidth();
-            int h = img.getHeight();
-            if (w != S || h != S)
-            {
-                double dw = 16.0 / w;
-                double dh = 16.0 / h;
-                double d = dw <= dh ? dw : dh;
-                w *= d;
-                h *= d;
-                java.awt.Image tmp = img.getScaledInstance(w, h, java.awt.Image.SCALE_DEFAULT);
-                img = new java.awt.image.BufferedImage(S, S, java.awt.image.BufferedImage.TYPE_INT_ARGB);
-                img.createGraphics().drawImage(tmp, (S - w) >> 1, (S - h) >> 1, w, h, null);
-            }
-
-            String hash = Hash.hash(false);
-            java.io.FileOutputStream fos = new java.io.FileOutputStream(new java.io.File(icoPath, hash + '.' + ConsEnv.IMAGE_FORMAT));
-            javax.imageio.ImageIO.write(img, ConsEnv.IMAGE_FORMAT, fos);
-            fos.flush();
-            fos.close();
-
-            javax.swing.ImageIcon icon = new javax.swing.ImageIcon(img);
-            IcoLabel label = createIcoLabel(hash, Integer.toString(tb_IconGrid.getComponents().length), icon, listener);
-            tb_IconGrid.add(label);
-            tb_IconGrid.revalidate();
-            selectIcoLabel(label);
-            Util.setIcon(hash, icon);
+            icoModel.appendIcon(filePath, iconPath);
         }
         catch (Exception exp)
         {
@@ -284,35 +181,156 @@ public class IcoDialog extends javax.swing.JDialog
             return;
         }
     }
+    private javax.swing.JButton bt_Append;
+    private javax.swing.JButton bt_Select;
+    private javax.swing.JTable tb_IconGrid;
+}
 
-    private IcoLabel createIcoLabel(String key, String txt, javax.swing.ImageIcon ico, java.awt.event.ActionListener listener)
+class IcoModel extends javax.swing.table.AbstractTableModel
+{
+
+    private java.util.List<javax.swing.JLabel> iconList;
+    private int columnCount;
+    private int checked;
+
+    IcoModel()
     {
-        IcoLabel label = new IcoLabel();
+        iconList = new java.util.ArrayList<javax.swing.JLabel>();
+        iconList.add(newLabel(0, Util.getNone()));
+        columnCount = 5;
+    }
+
+    @Override
+    public int getRowCount()
+    {
+        return iconList.size() / columnCount;
+    }
+
+    @Override
+    public int getColumnCount()
+    {
+        return columnCount;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex)
+    {
+        if (iconList.size() < 1)
+        {
+            return null;
+        }
+        int index = rowIndex * columnCount + columnIndex;
+        if (index >= iconList.size())
+        {
+            return null;
+        }
+        return iconList.get(index);
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex)
+    {
+        return javax.swing.JLabel.class;
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex)
+    {
+        return false;
+    }
+
+    public synchronized void initIcon(java.io.File icoPath, String lastIcon)
+    {
+        if (icoPath == null)
+        {
+            icoPath = Util.icoPath;
+            if (!icoPath.exists())
+            {
+                icoPath.mkdirs();
+            }
+        }
+
+        java.io.File[] fileList = icoPath.listFiles(new AmonFF("[0-9a-z]{16}\\.png", false));
+        if (fileList == null)
+        {
+            return;
+        }
+
+        java.util.regex.Pattern pattern = java.util.regex.Pattern.compile("[0-9a-z]{16}");
+        int i = 1;
+        for (java.io.File file : fileList)
+        {
+            if (!file.isFile())
+            {
+                continue;
+            }
+            java.util.regex.Matcher matcher = pattern.matcher(file.getName());
+            if (!matcher.find())
+            {
+                continue;
+            }
+
+            String key = matcher.group();
+            iconList.add(newLabel(i, Util.getIcon(key)));
+            if (key.equalsIgnoreCase(lastIcon))
+            {
+                checked = i;
+            }
+            i += 1;
+        }
+    }
+
+    public synchronized void appendIcon(java.io.File filePath, java.io.File icoPath) throws Exception
+    {
+        java.io.FileInputStream fis = new java.io.FileInputStream(filePath);
+        java.awt.image.BufferedImage img = javax.imageio.ImageIO.read(fis);
+        fis.close();
+
+        final int S = 16;
+        int w = img.getWidth();
+        int h = img.getHeight();
+        if (w != S || h != S)
+        {
+            double dw = 16.0 / w;
+            double dh = 16.0 / h;
+            double d = dw <= dh ? dw : dh;
+            w *= d;
+            h *= d;
+            java.awt.Image tmp = img.getScaledInstance(w, h, java.awt.Image.SCALE_DEFAULT);
+            img = new java.awt.image.BufferedImage(S, S, java.awt.image.BufferedImage.TYPE_INT_ARGB);
+            img.createGraphics().drawImage(tmp, (S - w) >> 1, (S - h) >> 1, w, h, null);
+        }
+
+        String hash = Hash.hash(false);
+        java.io.FileOutputStream fos = new java.io.FileOutputStream(new java.io.File(icoPath, hash + '.' + ConsEnv.IMAGE_FORMAT));
+        javax.imageio.ImageIO.write(img, ConsEnv.IMAGE_FORMAT, fos);
+        fos.flush();
+        fos.close();
+
+        javax.swing.ImageIcon icon = new javax.swing.ImageIcon(img);
+        Util.setIcon(hash, icon);
+        iconList.add(newLabel(iconList.size(), icon));
+    }
+
+    /**
+     * @param columnCount the columnCount to set
+     */
+    public void setColumnCount(int columnCount)
+    {
+        if (columnCount > 0)
+        {
+            this.columnCount = columnCount;
+        }
+    }
+
+    private javax.swing.JLabel newLabel(int num, javax.swing.Icon ico)
+    {
+        javax.swing.JLabel label = new javax.swing.JLabel();
         label.setIcon(ico);
         label.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         label.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        label.setText(txt);
-        label.setBackground(bdColor);
-        label.setForeground(fdColor);
-        label.setOpaque(true);
-        label.putClientProperty("key", key);
-        label.addActionListener(listener);
+        label.setText(Integer.toString(num));
         return label;
     }
-
-    private void selectIcoLabel(IcoLabel label)
-    {
-        if (lb_LastIcon != null)
-        {
-            lb_LastIcon.setBackground(bdColor);
-            lb_LastIcon.setForeground(fdColor);
-        }
-        lb_LastIcon = label;
-        lb_LastIcon.setBackground(bsColor);
-        lb_LastIcon.setForeground(fsColor);
-    }
-    private javax.swing.JButton bt_Append;
-    private javax.swing.JButton bt_Select;
-    private javax.swing.JTable tb_IconGrid;
 }
