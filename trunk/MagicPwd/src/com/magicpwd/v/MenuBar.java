fa8844432c8fa5f4e65bb83b54363a1352ee7f39
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-05-23 15:27:58 +0000

    云操作繁忙提示功能。

--- trunk/MagicPwd/src/com/magicpwd/v/MenuBar.java
+++ trunk/MagicPwd/src/com/magicpwd/v/MenuBar.java
@@ -509,6 +509,7 @@ public class MenuBar extends JMenuBar
         mu_DataMenu.add(mi_DataDocs);
 
         mi_DataSync = new javax.swing.JMenuItem();
+        mi_DataSync.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, InputEvent.CTRL_MASK));
         mi_DataSync.addActionListener(new java.awt.event.ActionListener()
         {
 
@@ -521,6 +522,7 @@ public class MenuBar extends JMenuBar
         mu_DataMenu.add(mi_DataSync);
 
         mi_DataBack = new javax.swing.JMenuItem();
+        mi_DataBack.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.CTRL_MASK));
         mi_DataBack.addActionListener(new java.awt.event.ActionListener()
         {
 
