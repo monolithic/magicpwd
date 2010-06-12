80b231203ae16059db01c83ff27ca1b7512ccaa3
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-06-12 14:48:10 +0000

    系统架构调整。

---
+++ MagicPwd/src/com/magicpwd/_face/IFormView.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd._face;
+
+/**
+ *
+ * @author aven
+ */
+public interface IFormView
+{
+
+    void setVisible(boolean visible);
+
+    javax.swing.JFrame getForm();
+}
