09c6d6db871a8fa34de910f1b1f79d165923a126
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-06-12 10:30:24 +0000

    记事便签标题获取光标时选中所有文本功能。

--- MagicPwd/src/com/magicpwd/_docs/Google.java
+++ MagicPwd/src/com/magicpwd/_docs/Google.java
@@ -49,6 +49,7 @@ public class Google
         service = new SpreadsheetService("MagicPwd");
         service.setUserCredentials(user, pass);
         SpreadsheetEntry spreadsheet = listSpreadsheetFeed(name);
+        WorksheetEntry worksheet;
         java.net.URL cellFeedUrl = null;
         if (spreadsheet == null)
         {
@@ -64,7 +65,6 @@ public class Google
             }
 
             java.util.List<WorksheetEntry> list = spreadsheet.getWorksheets();
-            WorksheetEntry worksheet;
             if (list.size() > 0)
             {
                 worksheet = list.get(0);
@@ -81,7 +81,11 @@ public class Google
                 worksheet.setColCount(2);
                 service.insert(entry.getWorksheetFeedUrl(), worksheet);
             }
-            int s = worksheet.getRowCount();
+            cellFeedUrl = worksheet.getCellFeedUrl();
+        }
+        else
+        {
+            worksheet = spreadsheet.getWorksheets().get(0);
             cellFeedUrl = worksheet.getCellFeedUrl();
         }
 
