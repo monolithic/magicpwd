12b15d1a5d7e7e86cf4461d1b657a59f7a7c1ffb
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-06-26 13:00:48 +0000

    导出为卡片功能。

--- MagicPwd/src/com/magicpwd/_util/Card.java
+++ MagicPwd/src/com/magicpwd/_util/Card.java
@@ -1,6 +1,5 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *
  */
 package com.magicpwd._util;
 
@@ -14,21 +13,38 @@ import org.dom4j.io.SAXReader;
 
 /**
  *
- * @author aven
+ * @author Amon
  */
 public class Card
 {
 
-    public static void exportHtm(java.io.File src, java.io.File dst)
+    public static java.io.File exportHtm(java.io.File src, java.io.File dst) throws Exception
     {
-        try
+        Document doc = new SAXReader().read(src);
+
+        Node node = doc.selectSingleNode("/magicpwd/card/template-res");
+        String text;
+        if (node != null)
         {
-            Document doc = new SAXReader().read(src);
+            text = node.getText();
+            if (Util.isValidate(text))
+            {
+                File.copy(new java.io.File(text), dst, true);
+            }
         }
-        catch (Exception ex)
+
+        node = doc.selectSingleNode("/magicpwd/card/template-uri");
+        if (node == null)
         {
-            Logs.exception(ex);
+            return null;
         }
+        text = node.getText();
+        if (!Util.isValidate(text))
+        {
+            return null;
+        }
+
+        return text(text, dst, ".htm");
     }
 
     public static java.io.File exportTxt(java.io.File src, java.io.File dst) throws Exception
@@ -45,47 +61,78 @@ public class Card
             return null;
         }
 
-        java.io.InputStream stream = File.open4Read(text);
-        java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.InputStreamReader(stream));
-        StringBuffer buffer = new StringBuffer();
-        String line = reader.readLine();
-        while (line != null)
+        return text(text, dst, ".txt");
+    }
+
+    public static java.io.File exportPng(java.io.File src, java.io.File dst)
+    {
+        return null;
+    }
+
+    public static java.io.File exportSvg(java.io.File src, java.io.File dst) throws Exception
+    {
+        Document doc = new SAXReader().read(src);
+        Node node = doc.selectSingleNode("/magicpwd/card/template-uri");
+        if (node == null)
         {
-            buffer.append(line);
-            line = reader.readLine();
+            return null;
+        }
+        String text = node.getText();
+        if (!Util.isValidate(text))
+        {
+            return null;
         }
 
+        return text(text, dst, ".svg");
+    }
+
+    public static java.io.File exportAll(java.io.File src, java.io.File dst)
+    {
+        return null;
+    }
+
+    private static java.io.File text(String src, java.io.File dst, String ext) throws Exception
+    {
+        java.io.InputStream stream = File.open4Read(src);
+        if (stream == null)
+        {
+            return null;
+        }
+
+        java.io.InputStreamReader reader = new java.io.InputStreamReader(stream);
+
+        StringBuffer buffer = new StringBuffer();
+        char[] buf = new char[1024];
+        int len = reader.read(buf);
+        while (len >= 0)
+        {
+            buffer.append(buf, 0, len);
+            len = reader.read(buf);
+        }
+
+        reader.close();
+        stream.close();
+
         GridMdl gm = UserMdl.getGridMdl();
-        IEditItem item;
+        IEditItem item = gm.getItemAt(ConsEnv.PWDS_HEAD_META);
+        dst = new java.io.File(dst, item.getName() + ext);
+        if (!dst.exists())
+        {
+            dst.createNewFile();
+        }
+
         for (int i = ConsEnv.PWDS_HEAD_SIZE, j = gm.getRowCount(); i < j; i += 1)
         {
             item = gm.getItemAt(i);
             replace(buffer, '#' + item.getName() + '#', item.getData());
         }
-        item = gm.getItemAt(ConsEnv.PWDS_HEAD_META);
-        dst = new java.io.File(dst, item.getName() + ".txt");
-        if (!dst.exists())
-        {
-            dst.createNewFile();
-        }
+
         java.io.BufferedWriter writer = new java.io.BufferedWriter(new java.io.FileWriter(dst));
         writer.write(buffer.toString());
         writer.flush();
         writer.close();
 
         return dst;
-    }
-
-    public static void exportPng(java.io.File src, java.io.File dst)
-    {
-    }
-
-    public static void exportSvg(java.io.File src, java.io.File dst)
-    {
-    }
-
-    public static void exportAll(java.io.File src, java.io.File dst)
-    {
     }
 
     private static void replace(StringBuffer buf, String src, String dst)
