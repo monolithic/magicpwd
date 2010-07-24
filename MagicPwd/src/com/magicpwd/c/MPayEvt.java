2ef83346de8b9d5e190331d5eb9025da22e1f8f9
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-07-24 17:53:30 +0000

    事件驱动框架调整。

---
+++ MagicPwd/src/com/magicpwd/c/MPayEvt.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd.c;
+
+/**
+ *
+ * @author aven
+ */
+public interface MPayEvt extends MenuEvt
+{
+}
---
+++ MagicPwd/src/com/magicpwd/c/MPwdEvt.java
@@ -0,0 +1,87 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd.c;
+
+/**
+ *
+ * @author aven
+ */
+public interface MPwdEvt extends MenuEvt
+{
+
+    void editFindActionPerformed(java.awt.event.ActionEvent evt);
+
+    void editTextActionPerformed(java.awt.event.ActionEvent evt);
+
+    void editPwdsActionPerformed(java.awt.event.ActionEvent evt);
+
+    void editLinkActionPerformed(java.awt.event.ActionEvent evt);
+
+    void editMailActionPerformed(java.awt.event.ActionEvent evt);
+
+    void editDateActionPerformed(java.awt.event.ActionEvent evt);
+
+    void editAreaActionPerformed(java.awt.event.ActionEvent evt);
+
+    void editFileActionPerformed(java.awt.event.ActionEvent evt);
+
+    void editPrevActionPerformed(java.awt.event.ActionEvent evt);
+
+    void editNextActionPerformed(java.awt.event.ActionEvent evt);
+
+    void keysModeActionPerformed(java.awt.event.ActionEvent evt);
+
+    void keysNoteActionPerformed(java.awt.event.ActionEvent evt);
+
+    void listSkeyActionPerformed(java.awt.event.ActionEvent evt);
+
+    void listSascActionPerformed(java.awt.event.ActionEvent evt);
+
+    void kindApndActionPerformed(java.awt.event.ActionEvent evt);
+
+    void kindUpdtActionPerformed(java.awt.event.ActionEvent evt);
+
+    void kindDeltActionPerformed(java.awt.event.ActionEvent evt);
+
+    void viewEditActionPerformed(java.awt.event.ActionEvent evt);
+
+    void viewSideActionPerformed(java.awt.event.ActionEvent evt);
+
+    void viewFindActionPerformed(java.awt.event.ActionEvent evt);
+
+    void viewMenuActionPerformed(java.awt.event.ActionEvent evt);
+
+    void viewToolActionPerformed(java.awt.event.ActionEvent evt);
+
+    void viewInfoActionPerformed(java.awt.event.ActionEvent evt);
+
+    void viewPrevActionPerformed(java.awt.event.ActionEvent evt);
+
+    void viewNextActionPerformed(java.awt.event.ActionEvent evt);
+
+    void viewHideActionPerformed(java.awt.event.ActionEvent evt);
+
+    void dataImptActionPerformed(java.awt.event.ActionEvent evt);
+
+    void dataExptActionPerformed(java.awt.event.ActionEvent evt);
+
+    void dataSyncActionPerformed(java.awt.event.ActionEvent evt);
+
+    void dataBackActionPerformed(java.awt.event.ActionEvent evt);
+
+    void dataDocsActionPerformed(java.awt.event.ActionEvent evt);
+
+    void helpSKeyActionPerformed(java.awt.event.ActionEvent evt);
+
+    void userSwitchActionPerformed(java.awt.event.ActionEvent evt);
+
+    void userCreateActionPerformed(java.awt.event.ActionEvent evt);
+
+    void userUpdateActionPerformed(java.awt.event.ActionEvent evt);
+
+    void userSecretActionPerformed(java.awt.event.ActionEvent evt);
+
+    void skinChangeActionPerformed(java.awt.event.ActionEvent evt);
+}
