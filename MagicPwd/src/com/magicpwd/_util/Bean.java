c10a94fd2cb0a53d7fef20cb6efc04cd198d35e5
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-08-01 14:00:58 +0000

    用户自定义菜单。

--- MagicPwd/src/com/magicpwd/_util/Bean.java
+++ MagicPwd/src/com/magicpwd/_util/Bean.java
@@ -11,6 +11,36 @@ package com.magicpwd._util;
 public class Bean
 {
 
+    /**
+     * 设置按钮的显示文本
+     * @param c
+     * @param wText
+     * @param isHash
+     */
+    public static void setText(javax.swing.AbstractButton c, String t)
+    {
+        int i = -1;
+        // 快捷字符替换
+        if (t.length() > 0)
+        {
+            i = t.indexOf('&');
+            if (i >= 0)
+            {
+                t = t.replace("&", "");
+                if (t.length() > i)
+                {
+                    c.setMnemonic(t.charAt(i));
+                }
+            }
+        }
+
+        if (!(i == 0 && t.length() == 1))
+        {
+            c.setText(t);
+            c.setDisplayedMnemonicIndex(i);
+        }
+    }
+
     public static void registerKeyStrokeAction(javax.swing.JComponent component, javax.swing.KeyStroke stroke, javax.swing.Action action, String command, int condition)
     {
         command = (command != null) ? command : ((action != null) ? action.toString() : "");
