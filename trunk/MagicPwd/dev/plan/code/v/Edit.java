4bee2145ee1436574ffaa8fa6ff4b117a079d16e
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-08-04 01:10:10 +0000

    设计包结构调整。

---
+++ trunk/MagicPwd/dev/plan/code/v/Edit.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plan.code.v;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Amon
+ */
+public class Edit extends JPanel {
+
+    private javax.swing.JButton bt_CopyData;
+    private javax.swing.JButton bt_SaveData;
+    private javax.swing.JButton bt_DropData;
+
+    public void init() {
+        bt_CopyData = new javax.swing.JButton();
+        bt_SaveData = new javax.swing.JButton();
+        bt_DropData = new javax.swing.JButton();
+
+        bt_CopyData.setText("C");
+
+        bt_SaveData.setText("S");
+
+        bt_DropData.setText("D");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        javax.swing.GroupLayout.ParallelGroup hgp = layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING);
+        hgp.addGap(0, 0, Short.MAX_VALUE);
+        hgp.addComponent(bt_DropData, javax.swing.GroupLayout.Alignment.TRAILING);
+        hgp.addComponent(bt_SaveData, javax.swing.GroupLayout.Alignment.TRAILING);
+        hgp.addComponent(bt_CopyData, javax.swing.GroupLayout.Alignment.TRAILING);
+        layout.setHorizontalGroup(hgp);
+
+        javax.swing.GroupLayout.SequentialGroup vsg = layout.createSequentialGroup();
+        vsg.addContainerGap(0, Short.MAX_VALUE);
+        vsg.addComponent(bt_CopyData);
+        vsg.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+        vsg.addComponent(bt_SaveData);
+        vsg.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+        vsg.addComponent(bt_DropData);
+        javax.swing.GroupLayout.ParallelGroup vpg = layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING);
+        vpg.addGroup(javax.swing.GroupLayout.Alignment.TRAILING, vsg);
+        layout.setVerticalGroup(vpg);
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Edit.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Edit.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Edit.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Edit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Edit m = new Edit();
+        m.init();
+
+        javax.swing.JFrame jf = new javax.swing.JFrame();
+        jf.getContentPane().add(m);
+        jf.pack();
+        jf.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+    }
+}
---
+++ trunk/MagicPwd/dev/plan/code/v/Find.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plan.code.v;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Amon
+ */
+public class Find extends javax.swing.JPanel {
+
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField jTextField1;
+
+    public void init() {
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+
+        jLabel1.setText("查找(X)");
+
+        jTextField1.setText("jTextField1");
+
+        jButton1.setText("jButton1");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+
+        javax.swing.GroupLayout.SequentialGroup hsg = layout.createSequentialGroup();
+        hsg.addComponent(jLabel1);
+        hsg.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+        hsg.addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 167, Short.MAX_VALUE);
+        hsg.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+        hsg.addComponent(jButton1);
+        layout.setHorizontalGroup(hsg);
+
+        javax.swing.GroupLayout.ParallelGroup vpg = layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE);
+        vpg.addComponent(jLabel1);
+        vpg.addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE);
+        vpg.addComponent(jButton1);
+
+        javax.swing.GroupLayout.SequentialGroup vsg = layout.createSequentialGroup();
+        vsg.addContainerGap();
+        vsg.addGroup(vpg);
+        vsg.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE);
+        layout.setVerticalGroup(vsg);
+    }
+
+    public static void main(String[] args) {
+        try {
+            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Find m = new Find();
+        m.init();
+        javax.swing.JFrame jf = new javax.swing.JFrame();
+        jf.getContentPane().add(m);
+        jf.pack();
+        jf.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+    }
+}
