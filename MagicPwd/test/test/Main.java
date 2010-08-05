dd01dcabac2dd2b28c39e943020201505e2aadc0
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-08-05 00:33:58 +0000

    Notepad OOP 设计参考。

---
+++ MagicPwd/test/test/Main.java
@@ -0,0 +1,27 @@
+package test;
+
+import com.magicpwd.v.MenuPtn;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Main
+{
+
+    public static void main(String args[])
+    {
+        MenuPtn ptn = new MenuPtn();
+        try
+        {
+            ptn.loadData("dat/menu.xml");
+        }
+        catch (Exception exp)
+        {
+            exp.printStackTrace();
+        }
+        JFrame frame = new JFrame();
+        frame.setJMenuBar(ptn.getMenuBar("Main"));
+        frame.setSize(new Dimension(400, 300));
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
