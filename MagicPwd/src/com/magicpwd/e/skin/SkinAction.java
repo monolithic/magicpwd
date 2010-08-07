9cfe4898d3fc4f06343d7bf578fca098a766ae82
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-08-07 22:54:02 +0000

    扩展皮肤支持功能。

---
+++ MagicPwd/src/com/magicpwd/e/skin/SkinAction.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd.e.skin;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+/**
+ *
+ * @author aven
+ */
+public class SkinAction extends AbstractAction
+{
+
+    @Override
+    public void actionPerformed(ActionEvent ae)
+    {
+    }
+}
