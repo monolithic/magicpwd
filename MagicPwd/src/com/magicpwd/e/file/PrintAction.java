f24913ac405da7386cf353bb745662c91371af18
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-08-13 12:27:10 +0000

    扩展皮肤支持功能。

---
+++ MagicPwd/src/com/magicpwd/e/file/PrintAction.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd.e.file;
+
+/**
+ *
+ * @author Amon
+ */
+public class PrintAction
+{
+}
