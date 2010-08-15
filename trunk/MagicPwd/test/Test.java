20816ee6bbd9d954f043aeecc955cba16a242374
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-08-15 10:21:37 +0000

    扩展皮肤支持功能。

---
+++ trunk/MagicPwd/test/Test.java
@@ -0,0 +1,18 @@
+
+import java.util.regex.Pattern;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aven
+ */
+public class Test {
+    public static void main(String[] args)
+    {
+        System.out.println(Pattern.matches("^(?!default).+$", "default"));
+    }
+}
