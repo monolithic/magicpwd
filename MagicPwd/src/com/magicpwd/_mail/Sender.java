5d160e471eb7a0a24da13e0a244c553db45535e9
amon.wk <amon.wk@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-08-19 10:23:01 +0000

    自定义风格功能。

--- MagicPwd/src/com/magicpwd/_mail/Sender.java
+++ MagicPwd/src/com/magicpwd/_mail/Sender.java
@@ -9,7 +9,6 @@ import java.util.Date;
 import java.util.List;
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
-import javax.mail.Address;
 import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.Multipart;
