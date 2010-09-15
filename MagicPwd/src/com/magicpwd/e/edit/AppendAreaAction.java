868177ed7529f9178d814266479157f3703053bb
amon.hm <amon.hm@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-09-15 07:52:55 +0000

    数据安全性完善。

--- MagicPwd/src/com/magicpwd/e/edit/AppendAreaAction.java
+++ MagicPwd/src/com/magicpwd/e/edit/AppendAreaAction.java
@@ -4,12 +4,26 @@
  */
 package com.magicpwd.e.edit;
 
+import com.magicpwd._cons.ConsDat;
+import com.magicpwd._cons.ConsEnv;
+import com.magicpwd.m.UserMdl;
+import com.magicpwd.v.MainPtn;
+
 /**
  *
  * @author Amon
  */
 public class AppendAreaAction extends javax.swing.AbstractAction
 {
+
+    private MainPtn mainPtn;
+    private UserMdl coreMdl;
+
+    public AppendAreaAction(MainPtn mainPtn, UserMdl coreMdl)
+    {
+        this.mainPtn = mainPtn;
+        this.coreMdl = coreMdl;
+    }
 
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e)
