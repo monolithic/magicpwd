467cccc985c5c306dc18aa60308da9ee43fb7fec
amon.hm <amon.hm@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-09-16 13:15:44 +0000

    数据安全性完善。

---
+++ trunk/MagicPwd/src/com/magicpwd/v/pwd/FindBar.java
@@ -0,0 +1,128 @@
+/**
+ * 
+ */
+package com.magicpwd.v.pwd;
+
+import com.magicpwd._cons.LangRes;
+import com.magicpwd._util.Lang;
+import com.magicpwd.m.ListMdl;
+import java.awt.event.FocusEvent;
+
+/**
+ * @author Amon
+ * 
+ */
+public class FindBar extends javax.swing.JPanel
+{
+
+    private MainPtn mainPtn;
+    private ListMdl listMdl;
+
+    public FindBar(MainPtn mainPtn, ListMdl listMdl)
+    {
+        this.mainPtn = mainPtn;
+        this.listMdl = listMdl;
+    }
+
+    public void initView()
+    {
+        lb_ItemFind = new javax.swing.JLabel();
+        tf_ItemFind = new javax.swing.JTextField();
+        bt_ItemFind = new javax.swing.JButton();
+
+        lb_ItemFind.setLabelFor(tf_ItemFind);
+
+        tf_ItemFind.addFocusListener(new java.awt.event.FocusListener()
+        {
+
+            @Override
+            public void focusGained(FocusEvent e)
+            {
+                tf_ItemFind.selectAll();
+            }
+
+            @Override
+            public void focusLost(FocusEvent e)
+            {
+            }
+        });
+
+        java.awt.event.ActionListener listener = new java.awt.event.ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                findActionPerformed(evt);
+            }
+        };
+        tf_ItemFind.addActionListener(listener);
+        bt_ItemFind.addActionListener(listener);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        javax.swing.GroupLayout.SequentialGroup hsg = layout.createSequentialGroup();
+        hsg.addComponent(lb_ItemFind);
+        hsg.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+        hsg.addComponent(tf_ItemFind, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE);
+        hsg.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+        hsg.addComponent(bt_ItemFind);
+        layout.setHorizontalGroup(hsg);
+
+        javax.swing.GroupLayout.ParallelGroup vpg = layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE);
+        vpg.addComponent(lb_ItemFind);
+        vpg.addComponent(bt_ItemFind);
+        vpg.addComponent(tf_ItemFind, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE);
+        layout.setVerticalGroup(vpg);
+    }
+
+    public void initLang()
+    {
+        Lang.setWText(lb_ItemFind, LangRes.P30F7301, "查找(@X)");
+
+        Lang.setWText(bt_ItemFind, LangRes.P30F7302, "查询(@Q)");
+    }
+
+    public void initData()
+    {
+        tf_ItemFind.requestFocus();
+    }
+
+    @Override
+    public void requestFocus()
+    {
+        tf_ItemFind.requestFocus();
+    }
+
+    public String getSearchText()
+    {
+        return tf_ItemFind.getText();
+    }
+
+    public void setSearchText(String text)
+    {
+        tf_ItemFind.setText(text);
+    }
+
+    private void findActionPerformed(java.awt.event.ActionEvent evt)
+    {
+
+        String text = getSearchText();
+        if (!com.magicpwd._util.Char.isValidate(text))
+        {
+            Lang.showMesg(this, LangRes.P30F7A18, "请输入您要查询的关键字，多个关键字可以使用空格或加号进行分隔！");
+            requestFocus();
+            return;
+        }
+
+        boolean b = listMdl.findName(text);
+        if (!b)
+        {
+            Lang.showMesg(this, LangRes.P30F7A19, "查询不到符合您条件的数据，请用空格或加号分隔您的搜索关键字后重试！");
+            requestFocus();
+        }
+    }
+    private javax.swing.JButton bt_ItemFind;
+    private javax.swing.JLabel lb_ItemFind;
+    private javax.swing.JTextField tf_ItemFind;
+}
---
+++ trunk/MagicPwd/src/com/magicpwd/v/pwd/ToolBar.java
@@ -0,0 +1,174 @@
+/**
+ * 
+ */
+package com.magicpwd.v.pwd;
+
+import javax.swing.JToolBar;
+
+import com.magicpwd._cons.ConsEnv;
+import com.magicpwd._cons.LangRes;
+import com.magicpwd._util.Lang;
+import com.magicpwd._util.Util;
+import com.magicpwd.c.ToolEvt;
+import com.magicpwd.m.UserCfg;
+
+/**
+ * @author Amon
+ */
+public class ToolBar extends JToolBar
+{
+
+    private UserCfg userCfg;
+
+    public ToolBar(UserCfg userCfg)
+    {
+        this.userCfg = userCfg;
+    }
+/*
+    public void initView()
+    {
+        bt_ApndData = new javax.swing.JButton();
+        bt_ApndData.setIcon(Util.getIcon(ConsEnv.ICON_KEYS_APND));
+        bt_ApndData.addActionListener(new java.awt.event.ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                te_ToolEvent.fileApndActionPerformed(evt);
+            }
+        });
+        add(bt_ApndData);
+
+        bt_SaveData = new javax.swing.JButton();
+        bt_SaveData.setIcon(Util.getIcon(ConsEnv.ICON_KEYS_SAVE));
+        bt_SaveData.addActionListener(new java.awt.event.ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                te_ToolEvent.fileSaveActionPerformed(evt);
+            }
+        });
+        add(bt_SaveData);
+
+        addSeparator();
+
+        bt_MovePrev = new javax.swing.JButton();
+        bt_MovePrev.setIcon(Util.getIcon(ConsEnv.ICON_ITEM_PREV));
+        bt_MovePrev.addActionListener(new java.awt.event.ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                te_ToolEvent.editPrevActionPerformed(evt);
+            }
+        });
+        add(bt_MovePrev);
+
+        bt_MoveNext = new javax.swing.JButton();
+        bt_MoveNext.setIcon(Util.getIcon(ConsEnv.ICON_ITEM_NEXT));
+        bt_MoveNext.addActionListener(new java.awt.event.ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                te_ToolEvent.editNextActionPerformed(evt);
+            }
+        });
+        add(bt_MoveNext);
+
+        addSeparator();
+
+        bt_HistBack = new javax.swing.JToggleButton();
+        bt_HistBack.setIcon(Util.getIcon(ConsEnv.ICON_TOOL_HIST));
+        bt_HistBack.setSelected(true);
+        bt_HistBack.addActionListener(new java.awt.event.ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                te_ToolEvent.histBackActionPerformed(evt);
+            }
+        });
+        add(bt_HistBack);
+
+        bt_PropSide = new javax.swing.JToggleButton();
+        bt_PropSide.setIcon(Util.getIcon(ConsEnv.ICON_TOOL_TAIL));
+        bt_PropSide.setSelected(userCfg.isEditWnd());
+        bt_PropSide.addActionListener(new java.awt.event.ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                te_ToolEvent.viewSideActionPerformed(evt);
+            }
+        });
+        add(bt_PropSide);
+
+        addSeparator();
+
+        bt_HelpHelp = new javax.swing.JButton();
+        bt_HelpHelp.setIcon(Util.getIcon(ConsEnv.ICON_SOFT_HELP));
+        bt_HelpHelp.addActionListener(new java.awt.event.ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                te_ToolEvent.helpHelpActionPerformed(evt);
+            }
+        });
+        add(bt_HelpHelp);
+    }
+
+    public void initLang()
+    {
+        Lang.setWTips(bt_ApndData, LangRes.P30F7B01, "添加数据");
+        Lang.setWTips(bt_SaveData, LangRes.P30F7B02, "保存数据");
+
+        Lang.setWTips(bt_MovePrev, LangRes.P30F7B03, "向上移动");
+        Lang.setWTips(bt_MoveNext, LangRes.P30F7B04, "向下移动");
+
+        Lang.setWTips(bt_HistBack, LangRes.P30F7B05, "保存历史记录");
+        Lang.setWTips(bt_PropSide, LangRes.P30F7B06, "分离属性窗口");
+
+        Lang.setWTips(bt_HelpHelp, LangRes.P30F7B07, "帮助");
+    }
+
+    public void initData()
+    {
+    }
+
+    public void setToolEvent(ToolEvt event)
+    {
+        te_ToolEvent = event;
+    }
+
+    public void setHistBackSelected(boolean selected)
+    {
+        bt_HistBack.setSelected(selected);
+    }
+
+    public boolean isHistBackSelected()
+    {
+        return bt_HistBack.isSelected();
+    }
+
+    public void setPropSideSelected(boolean selected)
+    {
+        bt_PropSide.setSelected(selected);
+    }
+    private javax.swing.JButton bt_ApndData;
+    private javax.swing.JButton bt_SaveData;
+    private javax.swing.JButton bt_MovePrev;
+    private javax.swing.JButton bt_MoveNext;
+    private javax.swing.JToggleButton bt_HistBack;
+    private javax.swing.JToggleButton bt_PropSide;
+    private javax.swing.JButton bt_HelpHelp;*/
+}
