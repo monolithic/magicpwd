c86963be12f245846e20d3384b4e86c83fbfa438
amon.wk <amon.wk@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-09-16 04:38:27 +0000

    扩展皮肤支持。

--- trunk/MagicPwd/test/test/Main.java
+++ trunk/MagicPwd/test/test/Main.java
@@ -9,7 +9,7 @@ public class Main
 
     public static void main(String args[])
     {
-        MenuPtn ptn = new MenuPtn();
+        MenuPtn ptn = new MenuPtn(null);
         try
         {
             ptn.loadData("dat/menu.xml");
