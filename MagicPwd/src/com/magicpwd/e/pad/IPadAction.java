15076c13c396e72e9f9f328664a435f154c2281a
amon.hm <amon.hm@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-09-16 22:52:52 +0000

    数据安全性完善。

---
+++ MagicPwd/src/com/magicpwd/e/pad/IPadAction.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd.e.pad;
+
+import com.magicpwd.m.CoreMdl;
+import com.magicpwd.v.pad.MiniPtn;
+
+/**
+ *
+ * @author Amon
+ */
+public interface IPadAction
+{
+
+    void setMiniPtn(MiniPtn miniPtn);
+
+    void setCoreMdl(CoreMdl coreMdl);
+}
---
+++ MagicPwd/src/com/magicpwd/e/pwd/IPwdAction.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd.e.pwd;
+
+import com.magicpwd.m.CoreMdl;
+import com.magicpwd.v.pwd.MainPtn;
+
+/**
+ *
+ * @author aven
+ */
+public interface IPwdAction
+{
+
+    void setMainPtn(MainPtn mainPtn);
+
+    void setCoreMdl(CoreMdl coreMdl);
+}
