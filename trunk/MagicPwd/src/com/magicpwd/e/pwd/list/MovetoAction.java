9c2a9ea53559ccf99e2a395b708962324add21b5
amonsoft <amonsoft@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-09-22 12:16:04 +0000

    扩展皮肤支持功能。

--- trunk/MagicPwd/src/com/magicpwd/e/pwd/list/MovetoAction.java
+++ trunk/MagicPwd/src/com/magicpwd/e/pwd/list/MovetoAction.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.magicpwd.e.pwd.kind;
+package com.magicpwd.e.pwd.list;
 
 import com.magicpwd._cons.LangRes;
 import com.magicpwd._face.IBackCall;
