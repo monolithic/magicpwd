5f24fe9b0392acb67aefe5ca795e05d94317523a
amon.hm <amon.hm@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-10-07 13:59:07 +0000

    数据安全性完善。

--- trunk/MagicPwd/src/com/magicpwd/e/pwd/skin/LookAction.java
+++ trunk/MagicPwd/src/com/magicpwd/e/pwd/skin/LookAction.java
@@ -40,6 +40,7 @@ public class LookAction extends javax.swing.AbstractAction implements IPwdAction
         String type;
         String look;
         String name;
+        String deco;
 
         UserCfg cfg = coreMdl.getUserCfg();
         if (ConsCfg.DEF_SKIN_DEF.equals(command))
@@ -47,28 +48,31 @@ public class LookAction extends javax.swing.AbstractAction implements IPwdAction
             type = "java";
             look = ConsCfg.DEF_SKIN_DEF;
             name = ConsCfg.DEF_SKIN_DEF;
+            deco = "false";
         }
         else if (ConsEnv.SKIN_LOOK_SYSTEM.equals(command))
         {
             type = "java";
             look = ConsCfg.DEF_SKIN_SYS;
             name = ConsCfg.DEF_SKIN_SYS;
+            deco = "true";
         }
         else
         {
             String[] arr = command.split("[:,]");
-            if (arr == null || arr.length != 3)
+            if (arr == null || arr.length != 4)
             {
                 return;
             }
             type = arr[0];
             look = arr[1];
             name = arr[2];
-            cfg.setCfg(ConsCfg.CFG_SKIN_NAME, name);
+            deco = arr[3];
         }
         cfg.setCfg(ConsCfg.CFG_SKIN_TYPE, type);
         cfg.setCfg(ConsCfg.CFG_SKIN_LOOK, look);
         cfg.setCfg(ConsCfg.CFG_SKIN_NAME, name);
+        cfg.setCfg(ConsCfg.CFG_SKIN_DECO, deco);
 
         Lang.showMesg(TrayPtn.getCurrForm(), LangRes.P30FAA1B, "新外观将在重启后生效！\n注意：一些外观可能造成性能问题，如果您觉得所选择的外观使系统运行变慢，\n　　　请选择使用“默认”或“系统”外观！");
     }
