bce16bd485580b7d19c3610177d2a83e420cfe88
amon.wk <amon.wk@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-10-08 05:36:26 +0000

    扩展皮肤支持。

--- trunk/MagicPwd/src/com/magicpwd/v/MenuPtn.java
+++ trunk/MagicPwd/src/com/magicpwd/v/MenuPtn.java
@@ -483,7 +483,7 @@ public class MenuPtn
         WButtonGroup group = new WButtonGroup();
 
         java.util.Properties prop = new java.util.Properties();
-        java.io.InputStreamReader reader = null;
+        java.io.FileReader reader = null;
         for (java.io.File dir : dirs)
         {
             java.io.File amf = new java.io.File(dir, ConsEnv.SKIN_FEEL_FILE);
@@ -493,7 +493,7 @@ public class MenuPtn
             }
             try
             {
-                reader = new java.io.InputStreamReader(new java.io.FileInputStream(amf));
+                reader = new java.io.FileReader(amf);
                 prop.load(reader);
 
                 item = new javax.swing.JCheckBoxMenuItem(action);
