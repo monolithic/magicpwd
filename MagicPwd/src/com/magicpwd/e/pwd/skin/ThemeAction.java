51d3b5f81748d0722b8830b6448b1aca32232da2
amon.hm <amon.hm@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-10-10 02:29:10 +0000

    语言资源调整。

---
+++ MagicPwd/src/com/magicpwd/e/pwd/skin/ThemeAction.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd.e.pwd.skin;
+
+import com.magicpwd.e.pwd.IPwdAction;
+import com.magicpwd.m.CoreMdl;
+import com.magicpwd.v.pwd.MainPtn;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ThemeAction extends javax.swing.AbstractAction implements IPwdAction
+{
+
+    public ThemeAction()
+    {
+    }
+
+    @Override
+    public void actionPerformed(java.awt.event.ActionEvent e)
+    {
+    }
+
+    @Override
+    public void setMainPtn(MainPtn mainPtn)
+    {
+//        this.mainPtn = mainPtn;
+    }
+
+    @Override
+    public void setCoreMdl(CoreMdl coreMdl)
+    {
+//        this.coreMdl = coreMdl;
+    }
+
+    @Override
+    public void doUpdate(Object object)
+    {
+    }
+}
