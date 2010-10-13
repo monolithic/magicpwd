0804db13f582cce6095ebf9606f873d96adf900c
amon.hm <amon.hm@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-10-13 13:45:36 +0000

    系统微调。

---
+++ trunk/MagicPwd/src/com/magicpwd/v/cmd/TinyPtn.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd.v.cmd;
+
+/**
+ * 命令行模式
+ * @author Aven
+ */
+public class TinyPtn extends javax.swing.JFrame
+{
+}
