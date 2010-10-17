a0dc3b9889b6e8311865edce5c0ce8fc40ace73c
amon.hm <amon.hm@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-10-17 02:13:13 +0000

    系统微调。

---
+++ MagicPwd/src/com/magicpwd/__i/IEditItem.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd.__i;
+
+/**
+ *
+ * @author Amon
+ */
+public interface IEditItem
+{
+
+    int getType();
+
+    void setType(int type);
+
+    String getName();
+
+    void setName(String name);
+
+    String getData();
+
+    boolean setData(String data);
+    ///////////////////////////////////////////////////////
+    // Spec属性
+    ///////////////////////////////////////////////////////
+    /**
+     * 附加属性索引
+     */
+    int SPEC_GUID_TPLT = 0;// 口令模板索引
+    int SPEC_FILE_NAME = 0;// 附件原文件名
+    int SPEC_PWDS_HASH = 0;// 字符空间索引
+    int SPEC_PWDS_SIZE = 1;// 生成口令长度
+    int SPEC_PWDS_LOOP = 2;// 是否允许重复
+    int SPEC_DATE_FORM = 0;// 日期显示格式
+
+    /**
+     * 
+     * @param spec
+     */
+    void addSpec(String spec);
+
+    String getSpec(int index);
+
+    String getSpec(int index, String defValue);
+
+    void setSpec(int index, String spec);
+
+    String enCodeSpec(String c);
+
+    void deCodeSpec(String text, String c);
+
+    void setDefault();
+}
