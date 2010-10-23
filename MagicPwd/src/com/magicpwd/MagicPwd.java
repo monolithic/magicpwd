e86db5c762fdaf3775b87648edda1e71f2620703
amon.hm <amon.hm@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-10-23 04:55:59 +0000

    系统微调。

--- MagicPwd/src/com/magicpwd/MagicPwd.java
+++ MagicPwd/src/com/magicpwd/MagicPwd.java
@@ -4,6 +4,7 @@ import com.magicpwd._cons.ConsEnv;
 import com.magicpwd._util.Jzip;
 import com.magicpwd._util.Lang;
 import com.magicpwd._util.Logs;
+import com.magicpwd.m.UserMdl;
 import com.magicpwd.v.TrayPtn;
 
 /**
@@ -22,17 +23,22 @@ public class MagicPwd
     public static void main(String[] args)
     {
         // 界面启动参数读取
-//        if (args != null && args.length > 0 && "webstart".equalsIgnoreCase(args[0]))
-//        {
-//            UserMdl.setRunMode(ConsEnv.MODE_RUN_WEB);
-//        }
+        if (args != null && args.length > 0)
+        {
+            String arg = args[0];
+            if ("webstart".equalsIgnoreCase(arg) || "web".equalsIgnoreCase(arg))
+            {
+                UserMdl.setRunMode(ConsEnv.RUN_MODE_WEB);
+            }
+            else if ("command".equalsIgnoreCase(arg) || "cmd".equalsIgnoreCase(arg))
+            {
+                UserMdl.setRunMode(ConsEnv.RUN_MODE_CMD);
+            }
+        }
 
         // 数据完整性处理
         zipData();
 
-        TrayPtn.getInstance().loadCfg();
-
-        // 界面风格设置
         try
         {
             javax.swing.SwingUtilities.invokeLater(new Runnable()
@@ -41,7 +47,9 @@ public class MagicPwd
                 @Override
                 public void run()
                 {
-                    TrayPtn.getInstance().loadLnf();
+                    // 加载用户配置
+                    TrayPtn.getInstance().loadCfg();
+
                     TrayPtn.getInstance().showViewPtn(ConsEnv.VIEW_MAIN);
                 }
             });
