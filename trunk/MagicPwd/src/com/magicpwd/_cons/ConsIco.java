ba99c26c473ded6452d58e092ba8107996e3ff41
amon.hm <amon.hm@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-10-25 13:02:21 +0000

    系统微调。

--- trunk/MagicPwd/src/com/magicpwd/_cons/ConsIco.java
+++ trunk/MagicPwd/src/com/magicpwd/_cons/ConsIco.java
@@ -22,4 +22,27 @@ package com.magicpwd._cons;
  */
 public interface ConsIco
 {
+
+    /**
+     * FILE
+     */
+    String FILE_NEW = "file-new";
+    String FILE_OPEN = "file-open";
+    String FILE_SAVE = "file-save";
+    String FILE_SAVEAS = "file-saveas";
+    String FILE_PRINT = "file-print";
+    String FILE_CLOSE = "file-close";
+    String FILE_EXIT = "file-exit";
+    /**
+     * EDIT
+     */
+    String EDIT_UNDO = "edit-undo";
+    String EDIT_REDO = "edit-redo";
+    String EDIT_DELETE_CUR = "edit-delete-cur";
+    String EDIT_DELETE_ALL = "edit-delete-all";
+    String EDIT_MOVE_UP = "edit-move-up";
+    String EDIT_MOVE_DOWN = "edit-move-down";
+    String EDIT_MOVE_LEFT = "edit-move-left";
+    String EDIT_MOVE_RIGHT = "edit-move-right";
+    String EDIT_HISTORY = "edit-history";
 }
