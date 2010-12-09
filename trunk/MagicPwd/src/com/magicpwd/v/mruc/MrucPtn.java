5af0407ed32e1c36f347cf5886917c5df44b0d4c
amon.wk <amon.wk@5271e558-d642-11de-9196-2db6e9c7c7ae>
2010-12-09 01:43:12 +0000

    1、修正Linux下窗口不能正常隐藏的问题； 2、修正部分用户交互细节问题； 3、添加特别致谢信息。

---
+++ trunk/MagicPwd/src/com/magicpwd/v/mruc/MrucPtn.java
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (C) 2010 Amon
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.magicpwd.v.mruc;
+
+import com.magicpwd.__a.AFrame;
+import com.magicpwd.m.UserMdl;
+import com.magicpwd.v.MenuPtn;
+import com.magicpwd.v.TrayPtn;
+
+/**
+ * 单位换算
+ * 
+ * @author Amon
+ */
+public class MrucPtn extends AFrame
+{
+
+    public MrucPtn(TrayPtn trayPtn, UserMdl userMdl)
+    {
+        super(trayPtn, userMdl);
+    }
+
+    @Override
+    public MenuPtn getMenuPtn()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean endSave()
+    {
+        return true;
+    }
+
+    @Override
+    public void requestFocus()
+    {
+    }
+}
