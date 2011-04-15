fb628c3af24c11e08facc411fc2b4c37e8d75de3
amon.wk <amon.wk@5271e558-d642-11de-9196-2db6e9c7c7ae>
2011-04-15 00:49:45 +0000

    魔方密码4.0.0.0

--- trunk/MagicPwd/src/com/magicpwd/v/mgtd/MgtdPtn.java
+++ trunk/MagicPwd/src/com/magicpwd/v/mgtd/MgtdPtn.java
@@ -17,14 +17,11 @@
 package com.magicpwd.v.mgtd;
 
 import com.magicpwd.__a.AMpwdPtn;
-import com.magicpwd._cons.LangRes;
 import com.magicpwd._util.Bean;
-import com.magicpwd._util.Lang;
 import com.magicpwd.m.UserMdl;
 import com.magicpwd.v.MenuPtn;
 import com.magicpwd.v.tray.TrayPtn;
 import com.magicpwd.x.mgtd.MgtdDlg;
-import java.awt.event.ActionEvent;
 
 /**
  *
@@ -41,23 +38,49 @@ public class MgtdPtn extends AMpwdPtn
     @Override
     public boolean initView()
     {
-        this.getContentPane().setLayout(new java.awt.FlowLayout());
-        b = new javax.swing.JButton();
-        b.addActionListener(new java.awt.event.ActionListener()
+        tbTaskTool = new javax.swing.JToolBar();
+        tbTaskTool.setRollover(true);
+        this.getContentPane().add(tbTaskTool, java.awt.BorderLayout.NORTH);
+
+        jButton1 = new javax.swing.JButton();
+        jButton1.setText("jButton1");
+        jButton1.setFocusable(false);
+        jButton1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jButton1.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jButton1.addActionListener(new java.awt.event.ActionListener()
         {
 
             @Override
-            public void actionPerformed(ActionEvent e)
+            public void actionPerformed(java.awt.event.ActionEvent e)
             {
                 test();
             }
         });
-        this.getContentPane().add(b);
+        tbTaskTool.add(jButton1);
+
+        plTaskList = new javax.swing.JPanel();
+        this.getContentPane().add(plTaskList, java.awt.BorderLayout.CENTER);
+
+        tbTaskList = new javax.swing.JTable();
+        javax.swing.JScrollPane spTaskList = new javax.swing.JScrollPane(tbTaskList);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(plTaskList);
+        plTaskList.setLayout(layout);
+        javax.swing.GroupLayout.SequentialGroup hsg = layout.createSequentialGroup();
+        hsg.addContainerGap();
+        hsg.addComponent(spTaskList, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE);
+        hsg.addContainerGap();
+        layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(hsg));
+
+        javax.swing.GroupLayout.SequentialGroup vsg = layout.createSequentialGroup();
+        vsg.addContainerGap();
+        vsg.addComponent(spTaskList, javax.swing.GroupLayout.DEFAULT_SIZE, 260, Short.MAX_VALUE);
+        vsg.addContainerGap();
+        layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(vsg));
 
         this.setIconImage(Bean.getLogo(16));
 
-        this.setSize(400, 300);
-//        this.pack();
+        this.pack();
         Bean.centerForm(this, null);
         this.setVisible(true);
         return true;
@@ -66,8 +89,6 @@ public class MgtdPtn extends AMpwdPtn
     @Override
     public boolean initLang()
     {
-        setTitle(Lang.getLang(LangRes.P30F6201, "魔方密码"));
-        b.setText("Test");
         return true;
     }
 
@@ -109,5 +130,8 @@ public class MgtdPtn extends AMpwdPtn
         dlg.initLang();
         dlg.initData();
     }
-    private javax.swing.JButton b;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JTable tbTaskList;
+    private javax.swing.JToolBar tbTaskTool;
+    private javax.swing.JPanel plTaskList;
 }
