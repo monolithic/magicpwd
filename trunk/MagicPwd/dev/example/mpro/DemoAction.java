e0e09425179e65d27013b692f240453c87319d65
amon.hm <amon.hm@5271e558-d642-11de-9196-2db6e9c7c7ae>
2011-09-24 13:25:49 +0000

    多用户功能。

--- trunk/MagicPwd/dev/example/mpro/DemoAction.java
+++ trunk/MagicPwd/dev/example/mpro/DemoAction.java
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package example.mpwd;
+package example.mpro;
 
 import com.magicpwd.__a.mpro.AMproAction;
 
