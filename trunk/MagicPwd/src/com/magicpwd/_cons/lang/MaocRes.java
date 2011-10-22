b9ddd26a19ddac38e1fc85313a4f70b971046cc8
amon.hm <amon.hm@5271e558-d642-11de-9196-2db6e9c7c7ae>
2011-10-22 02:35:05 +0000

    语言资源调整。

---
+++ trunk/MagicPwd/src/com/magicpwd/_cons/lang/MaocRes.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd._cons.lang;
+
+/**
+ * B 代数运算语言资源
+ * @author Aven
+ */
+public interface MaocRes
+{
+    // ----------------------------------------------------
+    // 2 标题语言资源
+    // ----------------------------------------------------
+    String P30FB200 = "P30FB200";//
+    String P30FB201 = "P30FB201";//
+    String P30FB202 = "P30FB202";//
+    String P30FB203 = "P30FB203";//
+    // ----------------------------------------------------
+    // 3 标签语言资源
+    // ----------------------------------------------------
+    String P30FB300 = "P30FB300";// c
+    String P30FB301 = "P30FB301";// 用户(@U)
+    String P30FB302 = "P30FB302";// 用户(@U)
+    String P30FB303 = "P30FB303";
+    String P30FB304 = "P30FB304";
+    String P30FB305 = "P30FB305";
+    String P30FB306 = "P30FB306";
+    String P30FB307 = "P30FB307";
+    String P30FB308 = "P30FB308";
+    String P30FB309 = "P30FB309";
+    String P30FB30A = "P30FB30A";
+    // ----------------------------------------------------
+    // 5 按钮语言资源
+    // ----------------------------------------------------
+    String P30FB500 = "P30FB500";
+    String P30FB501 = "P30FB501";
+    String P30FB502 = "P30FB502";
+    String P30FB503 = "P30FB503";
+    String P30FB504 = "P30FB504";
+    // ----------------------------------------------------
+    // 6 菜单语言资源
+    // ----------------------------------------------------
+    String P30FB600 = "P30FB600";
+    // ----------------------------------------------------
+    // A 消息提示语言资源
+    // ----------------------------------------------------
+    String P30FBA01 = "P30FBA01";
+    String P30FBA02 = "P30FBA02";
+    String P30FBA03 = "P30FBA03";
+    String P30FBA04 = "P30FBA04";
+    String P30FBA05 = "P30FBA05";
+    String P30FBA06 = "P30FBA06";
+    String P30FBA07 = "P30FBA07";
+    String P30FBA08 = "P30FBA08";
+    String P30FBA09 = "P30FBA09";
+    String P30FBA0A = "P30FBA0A";
+    String P30FBA0B = "P30FBA0B";
+    String P30FBA0C = "P30FBA0C";
+}
---
+++ trunk/MagicPwd/src/com/magicpwd/_cons/lang/NoneRes.java
@@ -0,0 +1,138 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magicpwd._cons.lang;
+
+/**
+ *
+ * @author Aven
+ */
+public interface NoneRes
+{
+    // ========================================================================
+    // A 自定义语言资源
+    // ========================================================================
+    // ----------------------------------------------------
+    // 1 共用语言资源
+    // ----------------------------------------------------
+    String P30FA100 = "P30FA100";// 7
+    String P30FA101 = "P30FA101";// ：
+    String P30FA102 = "P30FA102";// 创建时间
+    String P30FA103 = "P30FA103";// 口令名称
+    String P30FA104 = "P30FA104";// 关键搜索
+    String P30FA105 = "P30FA105";// 过期日期
+    String P30FA106 = "P30FA106";// 过期提示
+    String P30FA107 = "P30FA107";// 过期提示
+    String P30FA108 = "P30FA108";// 过期提示
+    String P30FA109 = "P30FA109";// 过期提示
+    String P30FA10A = "P30FA10A";// 过期提示
+    String P30FA10B = "P30FA10B";// 过期提示
+    String P30FA10C = "P30FA10C";// 过期提示
+    // ----------------------------------------------------
+    // 2 标题语言资源
+    // ----------------------------------------------------
+    String P30FA200 = "P30FA200";// 8
+    String P30FA201 = "P30FA201";// 用户登录
+    String P30FA202 = "P30FA202";// 身份认证
+    String P30FA203 = "P30FA203";// 用户注册
+    String P30FA204 = "P30FA204";// 口令找回
+    String P30FA205 = "P30FA205";// 登录口令修改
+    String P30FA206 = "P30FA206";// 安全口令修改
+    String P30FA207 = "P30FA207";// 添加从属用户
+    String P30FA208 = "P30FA208";// 友情提示
+    String P30FA209 = "P30FA209";// 数据升级
+    String P30FA20A = "P30FA20A";// 在线用户登录
+    String P30FA20B = "P30FA20B";// 配置Google Docs用户
+    // ----------------------------------------------------
+    // 3 标签语言资源
+    // ----------------------------------------------------
+    String P30FA300 = "P30FA300";// c
+    String P30FA301 = "P30FA301";// 用户(@U)
+    String P30FA302 = "P30FA302";// 口令(@P)
+    String P30FA303 = "P30FA303";// 确认(@R)
+    String P30FA304 = "P30FA304";// 现有口令(@O)
+    String P30FA305 = "P30FA305";// 修改口令(@N)
+    String P30FA306 = "P30FA306";// 口令确认(@R)
+    String P30FA307 = "P30FA307";// 升级
+    String P30FA308 = "P30FA308";// 升级
+    String P30FA309 = "P30FA309";// 旧版目录(@P)
+    String P30FA30A = "P30FA30A";// 旧版用户(@U)
+    String P30FA30B = "P30FA30B";// 旧版口令(@P)
+    String P30FA30C = "P30FA30C";// 安全口令(@P)
+    String P30FA30D = "P30FA30D";// 设定(@S)
+    String P30FA30E = "P30FA30E";// 设定(@S)
+    String P30FA30F = "P30FA30F";// 设定(@S)
+    String P30FA310 = "P30FA310";// 设定(@S)
+    String P30FA311 = "P30FA311";// 设定(@S)
+    String P30FA312 = "P30FA312";
+    String P30FA313 = "P30FA313";
+    // ----------------------------------------------------
+    // 5 按钮语言资源
+    // ----------------------------------------------------
+    String P30FA500 = "P30FA500";// c
+    String P30FA501 = "P30FA501";// 登录(@S)
+    String P30FA502 = "P30FA502";// 注册(@S)
+    String P30FA503 = "P30FA503";// 修改(@S)
+    String P30FA504 = "P30FA504";// 取消(@C)
+    String P30FA505 = "P30FA505";// 导入(@I)
+    String P30FA506 = "P30FA506";// >>
+    String P30FA507 = "P30FA507";// 请选择您要升级的旧版本数据根目录
+    String P30FA508 = "P30FA508";// 设定(@S)
+    String P30FA509 = "P30FA509";
+    String P30FA50A = "P30FA50A";// 确定(@O)
+    String P30FA50B = "P30FA50B";// 取消(@C)
+    String P30FA50C = "P30FA50C";// 选择(@C)
+    String P30FA50D = "P30FA50D";// 追加(@A)
+    String P30FA50E = "P30FA50E";//
+    String P30FA50F = "P30FA50F";// 徽标
+    // ----------------------------------------------------
+    // 6 菜单语言资源
+    // ----------------------------------------------------
+    String P30FA601 = "P30FA601";// "半小时"
+    String P30FA602 = "P30FA602";//
+    String P30FA603 = "P30FA603";// "1小时"
+    String P30FA604 = "P30FA604";//
+    String P30FA605 = "P30FA605";// "1天"
+    String P30FA606 = "P30FA606";//
+    String P30FA607 = "P30FA607";// "1周"
+    String P30FA608 = "P30FA608";//
+    String P30FA609 = "P30FA609";// "1月"
+    String P30FA60A = "P30FA60A";//
+    String P30FA60B = "P30FA60B";// "1年"
+    String P30FA60C = "P30FA60C";//
+    // ----------------------------------------------------
+    // A 消息提示语言资源
+    // ----------------------------------------------------
+    String P30FAA00 = "P30FAA00";// 13
+    String P30FAA01 = "P30FAA01";// 请输入用户名称！
+    String P30FAA02 = "P30FAA02";// 请输入登录口令！
+    String P30FAA03 = "P30FAA03";// 身份验证错误，请确认您的用户名及口令是否正确！
+    String P30FAA04 = "P30FAA04";// 系统错误：无法加载密码算法，请重新启动应用程序后重试！
+    String P30FAA05 = "P30FAA05";// 您输入的口令不一致，请重新输入！
+    String P30FAA06 = "P30FAA06";// 注册用户失败，请更换用户名及口令后重试！
+    String P30FAA07 = "P30FAA07";// 创建用户失败，请重新启动本程序尝试！
+    String P30FAA08 = "P30FAA08";// 新输入的两次口令不匹配，请重新输入！
+    String P30FAA09 = "P30FAA09";// 口令更改失败，请检查您输入的口令是否与原口令相同！
+    String P30FAA0A = "P30FAA0A";// 登录口令修改成功，您可以使用新口令登录了！
+    String P30FAA0B = "P30FAA0B";// 升级文件路径不能为空！
+    String P30FAA0C = "P30FAA0C";// 请选择一个合适的目录！
+    String P30FAA0D = "P30FAA0D";// 请选择旧版本软件存放的根目录！
+    String P30FAA0E = "P30FAA0E";// 数据升级成功！
+    String P30FAA0F = "P30FAA0F";// 数据升级出错！
+    String P30FAA10 = "P30FAA10";// 数据读取出错！
+    String P30FAA11 = "P30FAA11";// 确认要删除选中的历史数据么，此操作不可恢复？
+    String P30FAA12 = "P30FAA12";// 确认要删除此口令的所有历史记录吗，此操作将不可恢复？
+    String P30FAA13 = "P30FAA13";// 确认要恢复口令记录到选中的状态吗，此操作仅复制一份历史状态为当前状态？
+    String P30FAA14 = "P30FAA14";// 请输入安全口令！
+    String P30FAA15 = "P30FAA15";// 您输入的两次口令不匹配，请重新输入！
+    String P30FAA16 = "P30FAA16";// 安全口令设定失败，请稍后重新尝试！
+    String P30FAA17 = "P30FAA17";// 安全口令设定成功！
+    String P30FAA18 = "P30FAA18";// 您的新口令是：{0}！\\n为了您的安全，请登录软件后尽快修改您的口令。
+    String P30FAA19 = "P30FAA19";// 请选择您要移动的目标类别！
+    String P30FAA1A = "P30FAA1A";
+    String P30FAA1B = "P30FAA1B";
+    String P30FAA1C = "P30FAA1C";
+    String P30FAA1D = "P30FAA1D";
+    String P30FAA1E = "P30FAA1E";
+}
